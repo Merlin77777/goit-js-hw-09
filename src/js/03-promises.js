24f7dbc9108ac5d6e99d926480748c8a4ad3d0fa
Merlin <merlin.vop@gmail.com>
2023-04-16 23:46:54 +0300

    Завдання 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,39 @@
-console.log('task 3');
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmit);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.currentTarget.elements;
+
+  let promiceDelay;
+  for (let position = 0; position < amount.value; position++) {
+    promiceDelay = Number(delay.value) + position * Number(step.value);
+    createPromise(position, promiceDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
