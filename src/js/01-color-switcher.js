f0655e7d961c93cfe469a890aab58afedf7fb8d6
Merlin <merlin.vop@gmail.com>
2023-04-16 17:12:13 +0300

    Звідвння 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const button_start = document.querySelector('button[data-start]');
+button_start.addEventListener('click', onStart);
 
+const button_stop = document.querySelector('button[data-stop]');
+button_stop.addEventListener('click', onStop);
+button_stop.disabled = true;
+
+let timerId = null;
+
+function onStart() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  button_start.disabled = true;
+  button_stop.disabled = false;
+}
+
+function onStop() {
+  clearInterval(timerId);
+  button_start.disabled = false;
+  button_stop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+console.log('task 3');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
